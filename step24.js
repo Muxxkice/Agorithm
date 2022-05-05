d9244613998ddc7f1a1f745c5f2a8fad67702fcf
Mao Kurihara <mao.kurihara@gmail.com>
2022-05-06 00:21:48 +0900

    Answer Exam Randomly

---
+++ step24.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf8');
+const main = (input) => {
+	const n = input.split("\n")[0];
+	const list = input.split("\n").slice(1);
+
+	let value = 0;
+	list.forEach((n) => {
+		const q = n.split(" ")[0];
+		const p = n.split(" ")[1];
+		value += (p / q);
+	})
+	console.log(value);
+}
+main(require("fs").readFileSync("/dev/stdin", "utf-8"));
